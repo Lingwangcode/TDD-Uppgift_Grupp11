cee9de20f28af429a1e72ac51cf084340b201e4b
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2023-09-21 14:54:55 +0200

    hamcrest added and new test for Gear

--- src/main/java/com/example/demo/Gear.java
+++ src/main/java/com/example/demo/Gear.java
@@ -4,6 +4,7 @@ public enum Gear {
     DRIVE,
     REVERSE;
 
+    //not needed if we use hamcrest assertions
     public static boolean contains(Gear gear) {
         for (Gear enumValue : values()) {
             if (enumValue == gear) {
--- src/test/java/com/example/demo/GearTest.java
+++ src/test/java/com/example/demo/GearTest.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.in;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GearTest {
@@ -15,4 +18,15 @@ class GearTest {
     void reverseExists() {
         assertTrue(Gear.contains(Gear.REVERSE));
     }
+
+    //using hamcrest assertions to check the gears
+    @Test
+    void hasCorrectGears(){
+        assertThat(Gear.values(), arrayWithSize(2));
+        assertThat(Gear.REVERSE, in(Gear.values()));
+        assertThat(Gear.DRIVE, in(Gear.values()));
+        assertThat(null, not(in(Gear.values()))); // Should return false for null input
+        assertThat(Gear.class, not(in(Gear.values())));
+    }
+
 }
